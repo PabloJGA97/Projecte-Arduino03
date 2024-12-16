28cf4a53ce25e881d5e25ee59e001cf334dc508c
Pablo Fernandez <2023_pablo.fernandez@iticbcn.cat>
2024-12-16 01:13:06 +0100

    script bbdd complet

--- serveis_script.sh
+++ serveis_script.sh
@@ -1,30 +1,42 @@
 #!/bin/bash
 # Verificar l'estat de AWS IoT
+
 aws_iot_endpoint="a302ucw63g5l7h-ats.iot.us-east-1.amazonaws.com"
 status=$(aws iot describe-endpoint --endpoint-type iot:Data-ATS --output text --query 'endpointAddress')
+timestamp=$(date '+%Y-%m-%d %H:%M:%S')
 
 if [ "$status" == "$aws_iot_endpoint" ]; then
-    echo "AWS IoT está funcionando correctamente."
+    echo "$timestamp AWS IoT funciona correctament"
 else
-    echo "Alerta: AWS IoT no está disponible o el endpoint no coincide."
+    echo "$timestamp Alerta: AWS IoT no está disponible o ha canviat d'endpoint"
 fi
 
 
-# # Verificar el estado de la base de datos MySQL
-# mysql_host="localhost"
-# mysql_user="root"
-# mysql_password="1234"
-# mysqladmin ping -h "$mysql_host" -u "$mysql_user" -p"$mysql_password" > /dev/null 2>&1
-# if [ $? -eq 0 ]; then
-#     echo "La base de datos MySQL está funcionando correctamente."
-# else
-#     echo "Alerta: La base de datos MySQL no está disponible."
-# fi
-# # Verificar el estado del servidor Apache
-# apache_service="apache2"
-# systemctl is-active --quiet $apache_service
-# if [ $? -eq 0 ]; then
-#     echo "El servidor Apache está funcionando correctamente."
-# else
-#     echo "Alerta: El servidor Apache no está disponible."
-# fi
+# Verificar connectivitat i estat del servei de la base de dades PostgresSQL
+
+pg_host="192.168.33.8"
+pg_user="admin"  
+pg_password="c0still4!"  
+pg_database="project_bbdd"  
+export PGPASSWORD="$pg_password"
+
+bbdd_service="postgresql.service"
+
+# Revisa l'estatus del servei postgresql
+if systemctl is-active --quiet "$bbdd_service"; then
+    echo "$timestamp PostgreSQL servei està funcionant correctament."
+else
+    echo "$timestamp ALERTA: PostgreSQL servei NO està funcionant!"
+fi
+
+
+# Revisa que podem connectar-nos a la base de dades
+psql -h "$pg_host" -U "$pg_user" -d "$pg_database" -c "\dt;" > /dev/null 2>&1
+
+if [ $? -eq 0 ]; then
+    echo "$timestamp la base de dades '$pg_database' està funcionant i té connectivitat."
+else
+    echo "$timestamp ALERTA: no es pot connecta a la base de dades '$pg_database'."
+fi
+
+
